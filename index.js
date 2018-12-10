c5e3e18548eade0e4657e70df9136195ecbe7457
slvtrs <sal@workithealth.com>
2018-12-10 14:22:53 -0500

    ViewPropTypes update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import {
   View,
   WebView,
   StyleSheet,
+  ViewPropTypes
 } from 'react-native';
 
 
@@ -22,7 +23,7 @@ class SignaturePad extends Component {
     defaultWidth: PropTypes.number,
     onChange: PropTypes.func,
     onError: PropTypes.func,
-    style: View.propTypes.style,
+    style: ViewPropTypes.style,
     penColor: PropTypes.string,
     dataURL: PropTypes.string,
   };
